055853e595ee68acfed9645649f5c90897c93e19
Ids Klijnsma <werk@idsklijnsma.nl>
2016-12-28 15:39:57 +0100

    Minor fix

--- Upload.php
+++ Upload.php
@@ -86,7 +86,7 @@
     if($status -> result === 'OK' && class_exists('FileHook')) {
         $ph = new FileHook();
         if(method_exists($ph, 'uploadFile')) {
-            $ph->uploadFile($remotename, $path);
+            $ph->uploadFile($remotedir, $path);
         }
     }
 
